c34d1be32fe26e47f3b9c824008a0b49e46b4c47
yawlad33 <yawlad33@gmail.com>
2021-06-27 12:36:23 +0300

    laba_7_2
    laba_7_2

---
+++ My_7_2/Source.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+using namespace std;
+
+
+void p(double* a, double* b, double* c) {
+	double naim, sr, naib;
+	if ((*a) < (*b) && (*a) < (*c)) {
+		naim = (*a);
+		sr = (*b);
+		naib = (*c);
+		if ((*b) > (*c)) {
+			swap(sr, naib);
+		}
+	}
+	else if ((*b) < (*c) && (*b) < (*a)) {
+		naim = (*b);
+		sr = (*a);
+		naib = (*c);
+		if ((*a) > (*c)) {
+			swap(sr, naib);
+		}
+	}
+	else if ((*c) < (*b) && (*c) < (*a)) {
+		naim = (*c);
+		sr = (*b);
+		naib = (*a);
+		if ((*b) > (*c)) {
+			swap(sr, naib);
+		}
+	}
+
+	(*a) = naim;
+	(*b) = sr;
+	(*c) = naib;
+}
+
+int main() {
+	setlocale(LC_ALL, "Rus");
+	double a, b, c;
+	cout << "¬ведите 3 числа: ";
+	cin >> a >> b >> c;
+
+	p(&a, &b, &c);
+
+	cout << "a: " << a << endl;
+	cout << "b: " << b << endl;
+	cout << "c: " << c << endl;
+
+
+
+
+	return 0;
+}
